b582ab05387ad56e9b1fa42418936a9eceabd327
DanielH7-ctrl <danielhuerta826@gmail.com>
2022-03-06 12:07:29 -0500

    OnCreate

--- app/src/main/java/com/example/ciclodevida/MainActivity.kt
+++ app/src/main/java/com/example/ciclodevida/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.ciclodevida
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var nombre = "Daniel"
+    val NOMBRE = "nombre"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val boton = findViewById<Button>(R.id.boton)
+
+        //Toast.makeText(this, nombre, Toast.LENGTH_SHORT).show()
+
+        boton.setOnClickListener {
+            nombre = "Armando"
+            Toast.makeText(this, nombre, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        nombre = savedInstanceState.getString(NOMBRE)!!
+        Toast.makeText(this, nombre, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString(NOMBRE, nombre)
     }
 }
